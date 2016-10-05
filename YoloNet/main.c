0f45f289e5ad10adac2341444f040f4f6cb6cdc0
alec <azadikian1@hwemail.com>
2016-10-05 13:20:05 -0700

    Initial Commit

---
+++ YoloNet/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  YoloNet
+//
+//  Created by Alec Zadikian on 10/5/16.
+//  Copyright © 2016 AlecZ. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
